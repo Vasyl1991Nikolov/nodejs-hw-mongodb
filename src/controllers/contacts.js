c63b54656268434ddac61686484538055652d809
Vasyl Nikolov <vnnikolov1991@gmail.com>
2025-02-18 12:25:54 +0100

    post add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,7 @@ export const getContactByIdController = async (req, res) => {
 
 export const createContactController = async (req, res) => {
   const userId = req.user._id;
-  const contact = await createContact(...req.body, userId);
+  const contact = await createContact(req.body, userId);
 
   res.status(201).json({
     status: 201,
